0ce57129f599654d6d5f89bb8a642aa9ecf91802
Marucik <ziomekmrm@wp.pl>
2016-07-27 23:06:27 +0200

    Bez tekstu leeeel

---
+++ KeepoCO/js/script.js
@@ -0,0 +1,25 @@
+/* ##########################################  TEKSTY   ############################################### */
+	
+	function stronaglowna()
+{
+		document.getElementById("text").innerHTML="Strona główna";
+}
+				
+function projekty()
+{
+		document.getElementById("text").innerHTML="Projekty";
+}
+			
+function dzialalnosc()
+{
+		document.getElementById("text").innerHTML="Działalność";
+}
+						
+				
+function kontakt()
+{
+		document.getElementById("text").innerHTML="Kontakt";
+}
+
+
+		
